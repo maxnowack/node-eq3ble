d91a9f6885d560c20a4f12e14a45629cf7250a0d
Max Nowack <max@unsou.de>
2017-10-05 13:15:45 +0200

    fix linting

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import NobleDevice from 'noble-device'
+import NobleDevice from 'noble-device';
 import {
   serviceUuid,
   writeCharacteristic,
@@ -6,125 +6,127 @@ import {
   payload,
   parseInfo,
   parseProfile,
-} from './interface'
+} from './interface';
 
 class EQ3BLE {
   static SCAN_UUIDS = [serviceUuid]
   static is = peripheral => peripheral.advertisement.localName === 'CC-RT-BLE'
 
   constructor(device) {
-    NobleDevice.call(this, device)
-    this.notificationCallbacks = []
+    NobleDevice.call(this, device);
+    this.notificationCallbacks = [];
   }
   onNotify(...args) {
-    const callback = this.notificationCallbacks.shift()
+    const callback = this.notificationCallbacks.shift();
     if (!callback) {
-      this.emit('unhandledNotification', ...args)
-      return
+      this.emit('unhandledNotification', ...args);
+      return;
     }
-    callback(...args)
+    callback(...args);
   }
   getNextNotification() {
     return new Promise((resolve, reject) => {
-      let timeoutId
-      let removeCallback
+      let timeoutId;
+      let removeCallback;
       const callback = (...args) => {
-        clearTimeout(timeoutId)
-        removeCallback()
-        resolve(...args)
-      }
+        clearTimeout(timeoutId);
+        removeCallback();
+        resolve(...args);
+      };
       removeCallback = () => {
-        this.notificationCallbacks = this.notificationCallbacks.filter(cb => cb !== callback)
-      }
-      this.notificationCallbacks.push(callback)
+        this.notificationCallbacks = this.notificationCallbacks.filter(cb => cb !== callback);
+      };
+      this.notificationCallbacks.push(callback);
       setTimeout(() => {
-        removeCallback()
-        reject()
-      }, 1000)
-    })
+        removeCallback();
+        reject();
+      }, 1000);
+    });
   }
   writeAndGetNotification(data) {
     return new Promise((resolve, reject) => {
-      this.getNextNotification().then(resolve, reject)
+      this.getNextNotification().then(resolve, reject);
       this.writeDataCharacteristic(serviceUuid, writeCharacteristic, data, (err) => {
-        if (err) reject(err)
-      })
-    })
+        if (err) reject(err);
+      });
+    });
   }
 
   connectAndSetup() {
     return new Promise((resolve, reject) => {
       NobleDevice.prototype.connectAndSetup.call(this, (error) => {
         if (error) {
-          reject(error)
-          return
+          reject(error);
+          return;
         }
-        this.notifyCharacteristic(serviceUuid,
+        this.notifyCharacteristic(
+          serviceUuid,
           notificationCharacteristic,
           true,
           this.onNotify.bind(this),
           (err) => {
             if (err) {
-              reject(err)
-              return
+              reject(err);
+              return;
             }
-            resolve()
-          })
-      })
-    })
+            resolve();
+          },
+        );
+      });
+    });
   }
   getInfo() {
-    return this.writeAndGetNotification(payload.getInfo()).then(info => parseInfo(info))
+    return this.writeAndGetNotification(payload.getInfo()).then(info => parseInfo(info));
   }
   setBoost(enable) {
     if (enable) {
-      return this.writeAndGetNotification(payload.activateBoostmode())
+      return this.writeAndGetNotification(payload.activateBoostmode());
     }
-    return this.writeAndGetNotification(payload.deactivateBoostmode())
+    return this.writeAndGetNotification(payload.deactivateBoostmode());
   }
   automaticMode() {
-    return this.writeAndGetNotification(payload.setAutomaticMode())
+    return this.writeAndGetNotification(payload.setAutomaticMode());
   }
   manualMode() {
-    return this.writeAndGetNotification(payload.setManualMode())
+    return this.writeAndGetNotification(payload.setManualMode());
   }
   ecoMode() {
-    return this.writeAndGetNotification(payload.setEcoMode())
+    return this.writeAndGetNotification(payload.setEcoMode());
   }
   setLock(enable) {
     if (enable) {
-      return this.writeAndGetNotification(payload.lockThermostat())
+      return this.writeAndGetNotification(payload.lockThermostat());
     }
-    return this.writeAndGetNotification(payload.unlockThermostat())
+    return this.writeAndGetNotification(payload.unlockThermostat());
   }
   turnOff() {
-    return this.setTemperature(4.5)
+    return this.setTemperature(4.5);
   }
   turnOn() {
-    return this.setTemperature(30)
+    return this.setTemperature(30);
   }
   setTemperature(temperature) {
     return this.writeAndGetNotification(payload.setTemperature(temperature))
-      .then(info => parseInfo(info))
+      .then(info => parseInfo(info));
   }
   requestProfile(day) {
     return this.writeAndGetNotification(payload.requestProfile(day))
-      .then(profile => parseProfile(profile))
+      .then(profile => parseProfile(profile));
   }
   setProfile(day, periods) {
     return this.writeAndGetNotification(payload.setProfile(day, periods))
-      .then(result => result[0] === 2 && result[1] === 2)
+      .then(result => result[0] === 2 && result[1] === 2);
   }
   setTemperatureOffset(offset) {
-    return this.writeAndGetNotification(payload.setTemperatureOffset(offset))
+    return this.writeAndGetNotification(payload.setTemperatureOffset(offset));
   }
   updateOpenWindowConfiguration(temperature, duration) {
-    return this.writeAndGetNotification(payload.setWindowOpen(temperature, duration))
+    return this.writeAndGetNotification(payload.setWindowOpen(temperature, duration));
   }
   setDateTime(date) {
-    return this.writeAndGetNotification(payload.setDatetime(date)).then(info => parseInfo(info))
+    return this.writeAndGetNotification(payload.setDatetime(date)).then(info => parseInfo(info));
   }
 }
-NobleDevice.Util.inherits(EQ3BLE, NobleDevice)
+NobleDevice.Util.inherits(EQ3BLE, NobleDevice);
 
-export default EQ3BLE
+export default EQ3BLE;
--- src/interface.js
+++ src/interface.js
@@ -1,18 +1,18 @@
 /* eslint no-bitwise: 0 */
-export const writeCharacteristic = '3fa4585ace4a3baddb4bb8df8179ea09'
-export const notificationCharacteristic = 'd0e8434dcd290996af416c90f4e0eb2a'
-export const serviceUuid = '3e135142654f9090134aa6ff5bb77046'
+export const writeCharacteristic = '3fa4585ace4a3baddb4bb8df8179ea09';
+export const notificationCharacteristic = 'd0e8434dcd290996af416c90f4e0eb2a';
+export const serviceUuid = '3e135142654f9090134aa6ff5bb77046';
 
 export const payload = {
   getInfo: () => payload.setDatetime(new Date()),
-  activateBoostmode: () => new Buffer('4501', 'hex'),
-  deactivateBoostmode: () => new Buffer('4500', 'hex'),
-  setAutomaticMode: () => new Buffer('4000', 'hex'),
-  setManualMode: () => new Buffer('4040', 'hex'),
-  setEcoMode: () => new Buffer('4080', 'hex'),
-  lockThermostat: () => new Buffer('8001', 'hex'),
-  unlockThermostat: () => new Buffer('8000', 'hex'),
-  setTemperature: temperature => new Buffer(`41${temperature <= 7.5 ? '0' : ''}${(2 * temperature).toString(16)}`, 'hex'),
+  activateBoostmode: () => Buffer.from('4501', 'hex'),
+  deactivateBoostmode: () => Buffer.from('4500', 'hex'),
+  setAutomaticMode: () => Buffer.from('4000', 'hex'),
+  setManualMode: () => Buffer.from('4040', 'hex'),
+  setEcoMode: () => Buffer.from('4080', 'hex'),
+  lockThermostat: () => Buffer.from('8001', 'hex'),
+  unlockThermostat: () => Buffer.from('8000', 'hex'),
+  setTemperature: temperature => Buffer.from(`41${temperature <= 7.5 ? '0' : ''}${(2 * temperature).toString(16)}`, 'hex'),
   requestProfile: (day) => {
     const b = Buffer.alloc(2);
     b[0] = 32;
@@ -33,31 +33,31 @@ export const payload = {
     }
     return b;
   },
-  setTemperatureOffset: offset => new Buffer(`13${((2 * offset) + 7).toString(16)}`, 'hex'),
-  setDay: () => new Buffer('43', 'hex'),
-  setNight: () => new Buffer('44', 'hex'),
+  setTemperatureOffset: offset => Buffer.from(`13${((2 * offset) + 7).toString(16)}`, 'hex'),
+  setDay: () => Buffer.from('43', 'hex'),
+  setNight: () => Buffer.from('44', 'hex'),
   setComfortTemperatureForNightAndDay: (night, day) => {
-    const tempNight = (2 * night).toString(16)
-    const tempDay = (2 * day).toString(16)
-    return new Buffer(`11${tempDay}${tempNight}`, 'hex')
+    const tempNight = (2 * night).toString(16);
+    const tempDay = (2 * day).toString(16);
+    return Buffer.from(`11${tempDay}${tempNight}`, 'hex');
   },
   setWindowOpen: (temperature, minDuration) => {
-    const temp = (2 * temperature).toString(16)
-    const dur = (minDuration / 5).toString(16)
-    return new Buffer(`11${temp}${dur}`, 'hex')
+    const temp = (2 * temperature).toString(16);
+    const dur = (minDuration / 5).toString(16);
+    return Buffer.from(`11${temp}${dur}`, 'hex');
   },
   setDatetime: (date) => {
-    const b = Buffer.alloc(7)
-    b[0] = 3
-    b[1] = (date.getFullYear() % 100)
-    b[2] = (date.getMonth() + 1)
-    b[3] = date.getDate()
-    b[4] = date.getHours()
-    b[5] = date.getMinutes()
-    b[6] = date.getSeconds()
-    return b
+    const b = Buffer.alloc(7);
+    b[0] = 3;
+    b[1] = (date.getFullYear() % 100);
+    b[2] = (date.getMonth() + 1);
+    b[3] = date.getDate();
+    b[4] = date.getHours();
+    b[5] = date.getMinutes();
+    b[6] = date.getSeconds();
+    return b;
   },
-}
+};
 
 const status = {
   manual: 1,
@@ -68,12 +68,12 @@ const status = {
   unknown: 32,
   unknown2: 64,
   lowBattery: 128,
-}
+};
 
 export function parseInfo(info) {
-  const statusMask = info[2]
-  const valvePosition = info[3]
-  const targetTemperature = info[5] / 2
+  const statusMask = info[2];
+  const valvePosition = info[3];
+  const targetTemperature = info[5] / 2;
 
   return {
     status: {
@@ -86,7 +86,7 @@ export function parseInfo(info) {
     },
     valvePosition,
     targetTemperature,
-  }
+  };
 }
 
 export function parseProfile(buffer) {
@@ -94,6 +94,7 @@ export function parseProfile(buffer) {
   const periods = [];
   profile.periods = periods;
   if (buffer[0] === 33) {
+    // eslint-disable-next-line prefer-destructuring
     profile.dayOfWeek = buffer[1]; // 0-saturday, 1-sunday
     for (let i = 2; i < buffer.length; i += 2) {
       if (buffer[i] !== 0) {
@@ -102,7 +103,13 @@ export function parseProfile(buffer) {
         const toHuman = ((buffer[i + 1] * 10) / 60);
         const from = periods.length === 0 ? 0 : periods[periods.length - 1].to;
         const fromHuman = periods.length === 0 ? 0 : periods[periods.length - 1].toHuman;
-        periods.push({ temperature, from, to, fromHuman, toHuman });
+        periods.push({
+          temperature,
+          from,
+          to,
+          fromHuman,
+          toHuman,
+        });
       }
     }
   }
